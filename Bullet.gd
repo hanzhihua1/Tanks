1a86c1af41037c29a02078eaea2f766b9dc522df
Zhi <zhihuahan72@gmail.com>
2020-01-04 23:09:56 -0700

    add tileset

--- Bullet.gd
+++ Bullet.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-var speed = 750
+var speed = 350
 var velocity = Vector2()
+var Explosion = preload("res://Explode2.tscn")
 
 # Called when the node enters the scene tree for the first time.
 
@@ -18,6 +19,12 @@ func _physics_process(delta):
 			queue_free()
 		else:
 			velocity = velocity.bounce(collision.normal)
+			
+func hit():
+	var e = Explosion.instance()
+	e.start(position)
+	get_parent().add_child(e)
+	queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
     queue_free()
