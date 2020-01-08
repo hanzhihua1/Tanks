18a9af89e945972cb9811d6ed3194374d64d65ef
Zhi <zhihuahan72@gmail.com>
2020-01-07 22:05:44 -0700

    add basic raycast

--- Enemy1.gd
+++ Enemy1.gd
@@ -11,6 +11,8 @@ var Explosion = preload("res://Explode.tscn")
 var shootdelay = 800
 var time = OS.get_ticks_msec() + shootdelay
 
+var path = PoolVector2Array() setget set_path
+
 func get_input():
 	if get_parent().has_node("Player"):
 		$Sprite2.rotation = Player.position.angle_to_point(position) - PI/2
@@ -25,6 +27,7 @@ func get_input():
 		    velocity.y -= 1
 		velocity = velocity.normalized() * speed
 		$Sprite.rotation = velocity.normalized().angle() - PI/2
+		$RayCast2D.rotation = $Sprite2.rotation
 		
 		if time < OS.get_ticks_msec():
 			shoot()
@@ -37,12 +40,38 @@ func _physics_process(delta):
     velocity = move_and_slide(velocity)
 
 func shoot():
-	var b = Bullet.instance()
-	b.start($Sprite2.global_position + Vector2(40, 0).rotated($Sprite2.rotation + PI/2), $Sprite2.rotation + PI/2)
-	get_parent().add_child(b)
+	if $RayCast2D.get_collider() == Player:
+		var b = Bullet.instance()
+		b.start($Sprite2.global_position + Vector2(40, 0).rotated($Sprite2.rotation + PI/2), $Sprite2.rotation + PI/2)
+		get_parent().add_child(b)
 	
 func hit():
 	var e = Explosion.instance()
 	e.start(position)
 	get_parent().add_child(e)
 	queue_free()
+	
+func set_path(value):
+	path = value
+	if value.size() == 0:
+		return
+	set_process(true)
+
+func _process(delta):
+	return
+	
+func move_along_path(distance):
+	var start_point = position
+	for i in range(path.size()):
+		var distance_to_next = start_point.distance_to(path[0])
+		if distance <= distance_to_next and distance >= 0.0:
+			position = start_point.linear_interpolate(path[0], distance/distance_to_next)
+			break
+		elif distance < 0.0:
+			position = path[0]
+			set_process(false)
+			break
+		distance -= distance_to_next
+		start_point = path[0]
+		path.remove(0)
+	
