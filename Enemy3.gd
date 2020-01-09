c08a6b3311ecf396b925a3c7f68f3c58ed598339
Zhi <zhihuahan72@gmail.com>
2020-01-08 23:40:40 -0700

    all around raycasting

--- Enemy3.gd
+++ Enemy3.gd
@@ -16,13 +16,18 @@ var nav_time = OS.get_ticks_msec() + 500
 var path = PoolVector2Array() setget set_path
 
 func _ready():
+	var i = 0
+	for bundle in $Turret.get_children():
+		bundle.get_node("RayCast2D").rotation_degrees = i*360/14
+		i += 1
+	
 	set_process(false)
 
 func get_input():
 	if get_parent().has_node("Player"):
 		$Sprite2.rotation = Player.position.angle_to_point(position) - PI/2
 		$Sprite.rotation = velocity.normalized().angle() - PI/2
-		$RayCast2D3.rotation = $Sprite2.rotation
+		$FollowPlayer.rotation = $Sprite2.rotation
 		
 		if (bulletready == false) and time < OS.get_ticks_msec():
 			bulletready = true
@@ -35,7 +40,7 @@ func get_input():
 			
 		if nav_time < OS.get_ticks_msec():
 			nav_time = OS.get_ticks_msec() + 500
-			#velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()*speed
+			velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()*speed
 	else:
 		velocity = Vector2(0, 0)
 
@@ -44,23 +49,31 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity)
 	
 func aim():
-	$RayCast2D.rotation_degrees = OS.get_ticks_msec()/30
-	$RayCast2D2.global_position = $RayCast2D.get_collision_point()
-	
-	var angle = (position - $RayCast2D2.global_position).bounce($RayCast2D.get_collision_normal()).angle()
-	$RayCast2D2.rotation = angle + PI/2
+	for bundle in $Turret.get_children():
+		
+		var reflect_dir = (position - bundle.get_node("RayCast2D2").global_position).bounce(bundle.get_node("RayCast2D").get_collision_normal())
+		var angle = reflect_dir.angle()
+		
+		bundle.get_node("RayCast2D2").global_position = bundle.get_node("RayCast2D").get_collision_point() - 10*reflect_dir.normalized()
+		
+
+		bundle.get_node("RayCast2D2").rotation = angle + PI/2
 
 func shoot():
-	if ($RayCast2D.get_collider() == Player) or ($RayCast2D2.get_collider() == Player):
-		#Add some variance	
-		var b = Bullet.instance()
-		b.start($Sprite2.global_position + Vector2(40, 0).rotated($RayCast2D.rotation + PI/2), $RayCast2D.rotation + PI/2 + rand_range(-0.1, 0.1))
-		get_parent().add_child(b)
-	if ($RayCast2D3.get_collider() == Player):
+	if ($FollowPlayer.get_collider() == Player):
 		#Add some variance	
 		var b = Bullet.instance()
 		b.start($Sprite2.global_position + Vector2(40, 0).rotated($Sprite2.rotation + PI/2), $Sprite2.rotation + PI/2 + rand_range(-0.1, 0.1))
 		get_parent().add_child(b)
+		return
+	for bundle in $Turret.get_children():
+		if (bundle.get_node("RayCast2D2").get_collider() == Player):
+			#Add some variance	
+			var b = Bullet.instance()
+			b.start($Sprite2.global_position + Vector2(40, 0).rotated(bundle.get_node("RayCast2D").rotation + PI/2), bundle.get_node("RayCast2D").rotation + PI/2 + rand_range(-0.1, 0.1))
+			get_parent().add_child(b)
+			return
+
 	
 func hit():
 	var e = Explosion.instance()
