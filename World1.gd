7a8eb5e24a5a2eb9337026e151c0c6067c8ddd0f
Zhi <zhihuahan72@gmail.com>
2020-02-17 02:08:49 -0700

    cleanup

--- World1.gd
+++ World1.gd
@@ -6,9 +6,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func find_path_to_player(from_pos, to_pos):
 	return get_node('Navigation2D').get_simple_path(from_pos,to_pos, false)
